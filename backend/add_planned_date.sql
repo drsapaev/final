81e35fb67a39c1b6dbfdef3a32bbbc5c102d9e6d
Local Dev <dev@local.final>
2026-05-11 16:18:05 +0500

    chore(db): remove retired planned-date sql patch

--- backend/add_planned_date.sql
+++
@@ -1,5 +0,0 @@
--- Добавляем колонку planned_date в таблицу visits
-ALTER TABLE visits ADD COLUMN planned_date DATE;
-
--- Всем существующим визитам проставим сегодняшнюю дату
-UPDATE visits SET planned_date = DATE('now') WHERE planned_date IS NULL;
